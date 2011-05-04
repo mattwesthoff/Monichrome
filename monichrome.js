a0b31dabbca4a02dc3d39c8579a17484629d0fc3
Matt Westhoff <mattwesthoff@gmail.com>
2011-05-04 00:41:01 -0500

    making some progress

--- monichrome.js
+++ monichrome.js
@@ -1,38 +1,36 @@
-var chrome_getJSON = function(url, callback) {
-	console.log("sending getJSON request");
-	chrome.extension.sendRequest({action:'getJSON', url:url}, callback);
-};
-
 var Monichrome = function() {
 	var MonitisUrl = "http://www.monitis.com/api?apikey=23G3JB3995UOS78B3329UTB152&output=json&version=2&action=testsLastValues&locationIds=1,10";
 	return {
-		GetMonitors : function () {		
-			chrome_getJSON("dataservices.js", 
-				function(data) {
-					console.log("in the callback");
-					var items = [];
-					$.each(data, function(key, location) {
-						$.each(location, function(key, monitors) {
-							$.each(monitors, function (key, monitor) {
-								if (monitor.id) {
-									items.push('<tr><td>' + monitor.status + '</td><td>' + monitor.tag + '</td><td>' + monitor.name  + '</td></tr>');
-								}
-							});
-						});
+		Init : function () {
+			Monichrome.UpdateMonitors();
+		},
+		UpdateMonitors : function () {	
+			var data = chrome.extension.getBackgroundPage().MonitisPoller.GetMonitorData();
+			var items = [];
+			$.each(data, function(key, location) {
+				$.each(location, function(key, monitors) {
+					$.each(monitors, function (key, monitor) {
+						if (monitor.id) {
+							items.push('<tr><td>' + monitor.status + '</td><td>' + monitor.tag + '</td><td>' + monitor.name  + '</td></tr>');
+						}
 					});
-					var html = items.join('');
-					$('#monitor_data').html(html);
-					
-					var options = {
-						additionalFilterTriggers: [$('#quickfind')]
-					};
-					$('#monitors').tableFilter(options);
-				}
-			);
+				});
+			});
+			var html = items.join('');
+			$('#monitor_data').html(html);
+			
+			var options = {
+				additionalFilterTriggers: [$('#quickfind')]
+			};
+			$('#monitors').tableFilter(options);
+			
+			//$.uiTableFilter($('#monitors'), "0042");
+			
+			//setTimeout("Monichrome.UpdateMonitors()", 1000)
 		}
 	};
 }();
 
 $(function () {
-	Monichrome.GetMonitors();
+	Monichrome.Init();
 });
